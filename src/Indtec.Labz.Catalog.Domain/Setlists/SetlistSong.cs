a74620e2e288ea914fb28a63b91d642bdbf813b6
Felipe Fogaça <felipe.foogaca@gmail.com>
2026-08-20 18:05:10 -0300

    feat: support setlist song rehydration

--- src/Indtec.Labz.Catalog.Domain/Setlists/SetlistSong.cs
+++ src/Indtec.Labz.Catalog.Domain/Setlists/SetlistSong.cs
@@ -6,16 +6,16 @@ public sealed class SetlistSong
 {
     internal SetlistSong(Guid musicId, int position, TimeSpan duration, MusicalKey performanceKey)
     {
-        MusicId = musicId;
-        Position = position;
-        Duration = duration;
-        PerformanceKey = performanceKey;
+        MusicId = musicId; Position = position; Duration = duration; PerformanceKey = performanceKey;
     }
 
     public Guid MusicId { get; }
     public int Position { get; private set; }
     public TimeSpan Duration { get; }
     public MusicalKey PerformanceKey { get; }
+
+    public static SetlistSong Rehydrate(Guid musicId, int position, TimeSpan duration, MusicalKey performanceKey)
+        => new(musicId, position, duration, performanceKey);
 
     internal void MoveTo(int position) => Position = position;
 }
